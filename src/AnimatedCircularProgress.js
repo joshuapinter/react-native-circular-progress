87f9893eb8a11d0677c7e7d507a3c41554561598
Joshua Pinter <joshuapinter@gmail.com>
2016-08-12 12:56:46 -0600

    Timed animation with color change.

--- src/AnimatedCircularProgress.js
+++ src/AnimatedCircularProgress.js
@@ -7,8 +7,10 @@ export default class AnimatedCircularProgress extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-      chartFillAnimation: new Animated.Value(props.prefill || 0)
+      chartFillAnimation: new Animated.Value(props.prefill || 0),
+      tintColorPercentage: 0,
     }
   }
 
@@ -34,21 +36,60 @@ export default class AnimatedCircularProgress extends React.Component {
       }
     ).start();
   }
-  
+
   performLinearAnimation(toValue, duration) {
     Animated.timing(this.state.chartFillAnimation, {
       toValue: toValue,
       duration: duration
     }).start();
+
+    this.changeValueOverTime(this.state.tintColorPercentage, 0, 100, duration, 100);
+  }
+
+  changeValueOverTime( value, start, end, duration, framerate) {
+    var increment = ( ( end - start ) * framerate ) / duration;
+
+    var positive = ( end > start ? true : false );
+
+    var interval = setInterval( () => {
+      if ( ( positive && value >= end ) || ( !positive && value <= end ) ) {
+    	  clearInterval(interval);
+        return;
+      }
+
+    	value += increment;
+
+      this.setState({tintColorPercentage: value});
+
+    }, framerate);
+  }
+
+  hsl_col_perc(percent,start,end) {
+		 var fraction = percent / 100;
+
+     var change = ( end - start ) * fraction;
+
+     var hue = start + change;
+
+		return 'hsl(' + hue + ',100%,50%)';
   }
 
   render() {
     const { fill, prefill, ...other } = this.props;
 
+    console.log("tintColorPercentage2:");
+    console.log(this.state.tintColorPercentage);
+
+    let tintColor = this.hsl_col_perc(this.state.tintColorPercentage, 100, 0);
+
+    console.log("tintColor:");
+    console.log(tintColor);
+
     return (
       <AnimatedProgress
         {...other}
         fill={this.state.chartFillAnimation}
+        tintColor={tintColor}
         />
     )
   }
